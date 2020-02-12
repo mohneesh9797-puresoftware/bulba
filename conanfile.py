bae3c4a8e836bfde540425a79b8158e13f80130f
Igor Ermakov <408361+ermakov-i-o@users.noreply.github.com>
2020-02-13 01:18:03 +0300

    Trying apply DRY principle in CMakeLists.txt

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile
 
+
 class Bulba(ConanFile):
     name = "bulba"
     version = "0.1.0"
--- src/greeting.cpp
+++ src/greeting.cpp
@@ -7,7 +7,11 @@ namespace bulba {
 
 void greet(std::string name)
 {
-    std::thread{[name = std::move(name)] { std::cout << "Hello, " << name << '!' << std::endl; }}.join();
+    std::thread{[name = std::move(name)]
+        {
+            std::cout << "Hello, " << name << '!' << std::endl;
+        }
+    }.join();
 }
 
 } // namespace bulba
